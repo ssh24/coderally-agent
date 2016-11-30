41def4eb165b7b1686a31758966ac0c5f25fa09a
ssh24 <sadman.hasan99@live.com>
2016-11-30 12:15:10 -0500

    Updated agent code to look up u_id
    Updated gitignore to ignore node_modules
    Updated default.js to do a request to the user endpoint and get the user_id

--- examples/default.js
+++ examples/default.js
@@ -43,65 +43,83 @@ var AIUtils = Agent.AIUtils;
 
 var myAgent = new Agent();
 
-myAgent.enterRace({
-	track_id : "13",
-	username : "tom",
-	user_id : "1",
-	uniqueUserid : "",
-	file_name : "Testcar",
-	vehicle_type: "buggy_black",
-	accel : "1",
-	weight : "1",
-	armor : "0",
-	traction : "1",
-	turning : "1",
-	compressedDataStream: false
-}, 'challenge-one.coderallycloud.com');
+var http = require('http');
 
-// Agent logic
-myAgent.on('init', function (ourCar, track) {
-	console.log('init occurred');
+var info = {
+  track_id: "13",
+  username: "tom",
+  uniqueUserid: "",
+  file_name: "Testcar",
+  vehicle_type: "buggy_black",
+  accel: "1",
+  weight: "1",
+  armor: "0",
+  traction: "1",
+  turning: "1",
+  compressedDataStream: false
+};
+
+var options = {
+  host: 'challenge-one.coderallycloud.com',
+  path: '/CodeRallyWeb/GetUser?user_name=' + info.username
+};
+
+var req = http.request(options, function(response) {
+  var str = '';
+  response.on('data', function(chunk) {
+    str += chunk;
+  });
+  response.on('end', function() {
+    info.user_id = JSON.parse(str).user_id;
+
+    myAgent.enterRace(info, 'challenge-one.coderallycloud.com');
+    // Agent logic
+    myAgent.on('init', function(ourCar, track) {
+      console.log('init occurred');
+    });
+
+    myAgent.on('onRaceStart', function(ourCar) {
+      console.log("Race is starting");
+
+      // Aggressive start
+      var target = AIUtils.getClosestLane(ourCar.getCarStatus().getCheckPoint(), ourCar.getCarStatus().getPosition());
+      ourCar.pushCarControl({
+        carBrakePercent: 0,
+        carAccelPercent: 100,
+        carTarget: target
+      })
+
+    });
+
+    myAgent.on('onCheckpointUpdated', function(ourCar, checkpoint) {
+      var target = AIUtils.getClosestLane(ourCar.getCarStatus().getCheckPoint(), ourCar.getCarStatus().getPosition());
+      ourCar.pushCarControl({
+        carBrakePercent: 0,
+        carAccelPercent: 100,
+        carTarget: target
+      });
+      AIUtils.recalculateHeading(ourCar, 0.75);
+    });
+
+    myAgent.on('onStalled', function(ourCar) {
+      AIUtils.recalculateHeading(ourCar, 1);
+      ourCar.pushCarControl({
+        carAccelPercent: 100,
+        carBrakePercent: 0
+      });
+    });
+
+    myAgent.on('onTimeStep', function(ourCar) {
+      console.log("Position ", JSON.stringify(ourCar.getCarStatus().getPosition()));
+      console.log("Lap ", JSON.stringify(ourCar.getCarStatus().getLap()));
+      console.log("Place ", JSON.stringify(ourCar.getCarStatus().getPlace()));
+      console.log("Acceleration ", JSON.stringify(ourCar.getCarStatus().getAcceleration()));
+      console.log("Target ", JSON.stringify(ourCar.getCarStatus().getTarget()));
+      console.log("Checkpoint ", JSON.stringify(ourCar.getCarStatus().getCheckPoint()));
+      console.log(); // filler
+
+      AIUtils.recalculateHeading(ourCar, 1);
+    });
+  });
 });
-
-myAgent.on('onRaceStart', function (ourCar) {
-	console.log("Race is starting");
-
-	// Aggressive start
-	var target = AIUtils.getClosestLane(ourCar.getCarStatus().getCheckPoint(), ourCar.getCarStatus().getPosition());
-	ourCar.pushCarControl({
-		carBrakePercent : 0,
-		carAccelPercent : 100,
-		carTarget : target
-	})
-
-});
-
-myAgent.on('onCheckpointUpdated', function (ourCar, checkpoint) {
-	var target = AIUtils.getClosestLane(ourCar.getCarStatus().getCheckPoint(), ourCar.getCarStatus().getPosition());
-	ourCar.pushCarControl({
-		carBrakePercent : 0,
-		carAccelPercent : 100,
-		carTarget : target
-	});
-	AIUtils.recalculateHeading(ourCar, 0.75);
-});
-
-myAgent.on('onStalled', function (ourCar) {
-	AIUtils.recalculateHeading(ourCar, 1);
-	ourCar.pushCarControl({
-		carAccelPercent : 100,
-		carBrakePercent : 0
-	});
-});
-
-myAgent.on('onTimeStep', function (ourCar) {
-	console.log("Position ", JSON.stringify(ourCar.getCarStatus().getPosition()));
-	console.log("Lap ", JSON.stringify(ourCar.getCarStatus().getLap()));
-	console.log("Place ", JSON.stringify(ourCar.getCarStatus().getPlace()));
-	console.log("Acceleration ", JSON.stringify(ourCar.getCarStatus().getAcceleration()));
-	console.log("Target ", JSON.stringify(ourCar.getCarStatus().getTarget()));
-	console.log("Checkpoint ", JSON.stringify(ourCar.getCarStatus().getCheckPoint()));
-	console.log(); // filler
-
-	AIUtils.recalculateHeading(ourCar, 1);
-});
+req.end();
